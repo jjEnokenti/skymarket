dcfa4184177378ef565224e22cb5842cb16cc1c4
mgd <mr.zizikk@gamil.com>
2023-05-19 19:34:30 -0400

    add docs comment viewset, serializers

--- skymarket/ads/serializers/comment.py
+++ skymarket/ads/serializers/comment.py
@@ -4,12 +4,14 @@ from ads.models import Comment
 
 
 class CommentListSerializer(serializers.ModelSerializer):
+    """Comments list serializer."""
+
     pk = serializers.ReadOnlyField(source='id')
     author_first_name = serializers.ReadOnlyField(source='author.first_name')
     author_last_name = serializers.ReadOnlyField(source='author.last_name')
     author_id = serializers.ReadOnlyField(source='author.id')
     ad_id = serializers.ReadOnlyField(source='ad.id')
-    author_image = serializers.ImageField(source='author.image')
+    author_image = serializers.ImageField(source='author.image', read_only=True)
 
     class Meta:
         model = Comment
@@ -17,12 +19,18 @@ class CommentListSerializer(serializers.ModelSerializer):
 
 
 class CommentCreateSerializer(CommentListSerializer):
+    """Comment create serializer."""
+
     pass
 
 
 class CommentDetailSerializer(CommentListSerializer):
+    """Comment detail serializer."""
+
     pass
 
 
 class CommentUpdateSerializer(CommentListSerializer):
+    """Comment update serializer."""
+
     pass
--- skymarket/ads/views/comment.py
+++ skymarket/ads/views/comment.py
@@ -1,3 +1,4 @@
+from drf_spectacular.utils import extend_schema_view, extend_schema
 from rest_framework import viewsets
 from rest_framework.generics import get_object_or_404
 from rest_framework.permissions import IsAuthenticated
@@ -12,36 +13,73 @@ from ads.serializers.comment import (
 )
 
 
+@extend_schema_view(
+    list=extend_schema(
+        tags=['comments'],
+        responses=CommentListSerializer,
+        description='Comments list view',
+        summary='comments list'
+    ),
+    retrieve=extend_schema(
+        tags=['comments'],
+        responses=CommentDetailSerializer,
+        description='Comment detail view',
+        summary='comment detail'
+    ),
+    create=extend_schema(
+        tags=['comments'],
+        responses=CommentCreateSerializer,
+        description='Comment create view',
+        summary='comment create'
+    ),
+    partial_update=extend_schema(
+        tags=['comments'],
+        responses=CommentUpdateSerializer,
+        description='Comment partial update view',
+        summary='Comment partial update'
+    ),
+    destroy=extend_schema(
+        tags=['comments'],
+        description='Comment delete view',
+        summary='comment delete'
+    ),
+)
 class CommentViewSet(viewsets.ModelViewSet):
     queryset = Comment.objects.all()
+    http_method_names = ['get', 'post', 'patch', 'delete']
 
     default_serializer = CommentListSerializer
     serializers = {
         'retrieve': CommentDetailSerializer,
         'create': CommentCreateSerializer,
-        'update': CommentUpdateSerializer,
         'partial_update': CommentUpdateSerializer,
     }
 
     default_permission = [IsAuthenticated()]
     permissions = {
-        'update': [IsAuthenticated(), CanEditOrDelete()],
         'partial_update': [IsAuthenticated(), CanEditOrDelete()],
         'destroy': [IsAuthenticated(), CanEditOrDelete()]
     }
-
-    def list(self, request, *args, **kwargs):
-        self.queryset = Comment.objects.filter(ad=self.kwargs.get('ad_pk'))
-        return super().list(request, *args, **kwargs)
-
-    def perform_create(self, serializer):
-        user = self.request.user
-        ad_pk = self.kwargs.get('ad_pk')
-        ad = get_object_or_404(Ad, pk=ad_pk)
-        serializer.save(author=user, ad=ad)
 
     def get_serializer_class(self):
         return self.serializers.get(self.action, self.default_serializer)
 
     def get_permissions(self):
         return self.permissions.get(self.action, self.default_permission)
+
+    def get_queryset(self):
+        if self.action in ['retrieve', 'partial_update', 'destroy']:
+            return Comment.objects.filter(
+                ad=self.kwargs.get('ad_pk'),
+                pk=self.kwargs.get('pk')
+            )
+        elif self.action == 'list':
+            return Comment.objects.filter(
+                ad=self.kwargs.get('ad_pk')
+            )
+
+    def perform_create(self, serializer):
+        user = self.request.user
+        ad_pk = self.kwargs.get('ad_pk')
+        ad = get_object_or_404(Ad, pk=ad_pk)
+        serializer.save(author=user, ad=ad)
