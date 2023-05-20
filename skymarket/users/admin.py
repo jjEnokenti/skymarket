5709efdbc806c5b18594de56179317ec484aeac2
mgd <mr.zizikk@gamil.com>
2023-05-19 20:52:31 -0400

    remove poetry.lock pyproject.toml from repo

--- skymarket/users/admin.py
+++ skymarket/users/admin.py
@@ -1,11 +1,8 @@
 from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin
+# from django.contrib.auth.admin import UserAdmin
 
 from .models import User
 
-
-# TODO Aдмика для пользователя - как реализовать ее можно подсмотреть в документаци django
-# TODO Обычно её всегда оформляют, но в текущей задачи делать её не обязательно
 
 #
 # class UserAdminPanel(UserAdmin):
